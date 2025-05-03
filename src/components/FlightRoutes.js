92486a4dc0d8e7b93200ead76607e9a3aa5ec59f
Ashiqul Islam Shabu <ashiq.cse3.bu@gmail.com>
2025-05-03 16:41:47 +0600

    project all feature completed

--- src/components/FlightRoutes.js
+++ src/components/FlightRoutes.js
@@ -196,7 +196,7 @@ const TopRoutesSection = () => {
         </div>
       </div>
 
-      {/* Route Pairs in 3-column grid */}
+   
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
         {(activeTab === "Domestic" ? domesticRoutes : internationalRoutes).map(
           (route, index) => (
